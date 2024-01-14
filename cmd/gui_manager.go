61e8c1952c65e770a815124ad6f7539e51f692d2
nicolasmd87 <nicolasmd87@gmail.com>
2024-01-14 18:23:52 -0300

    we have a better debug config and object ids

--- cmd/gui_manager.go
+++ cmd/gui_manager.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"Gopher3D/renderer"
+	"path/filepath"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -12,24 +13,37 @@ import (
 )
 
 var modelChan = make(chan *renderer.Model)
+var objectNames []string // Global slice to store object names
+var list *widget.List    // List widget to display object names
 
 func main() {
 	app := app.New()
 	window := app.NewWindow("Gopher 3D")
 	gopher := NewGopher()
-	// Set window size to 1024x768
 	window.Resize(fyne.NewSize(1024, 768))
+
+	list = widget.NewList(
+		func() int {
+			return len(objectNames)
+		},
+		func() fyne.CanvasObject {
+			return widget.NewLabel("template")
+		},
+		func(id widget.ListItemID, co fyne.CanvasObject) {
+			co.(*widget.Label).SetText(objectNames[id])
+		},
+	)
 
 	loadItem := fyne.NewMenuItem("Load Object", func() {
 		fd := dialog.NewFileOpen(func(reader fyne.URIReadCloser, err error) {
 			if err == nil && reader == nil {
-				// User canceled the dialog
 				return
 			}
 			if err != nil {
 				dialog.ShowError(err, window)
 				return
 			}
+
 			defer reader.Close()
 
 			filePath := reader.URI().Path()
@@ -39,30 +53,29 @@ func main() {
 				return
 			}
 			modelChan <- model
+
+			objectName := filepath.Base(filePath) // Extract the file name as the object name
+			objectNames = append(objectNames, objectName)
+			list.Refresh() // Refresh the list to update the UI with the new object name
 		}, window)
 
-		fd.SetFilter(storage.NewExtensionFileFilter([]string{".obj"})) // For example, filter for ".obj" files
+		fd.SetFilter(storage.NewExtensionFileFilter([]string{".obj"}))
 		fd.Show()
 	})
 
-	// Create a main menu with a "File" drop-down containing the load item
 	mainMenu := fyne.NewMainMenu(
-		// A File menu
 		fyne.NewMenu("File", loadItem),
 	)
-
 	window.SetMainMenu(mainMenu)
 
-	// Create a box container (vbox) to place the label.
-	box := container.NewVBox(
-		widget.NewLabel("Welcome to the Gopher 3D Engine!"),
+	content := container.NewVBox(
+		widget.NewLabel("Loaded Objects:"),
+		list,
 	)
 
-	gap := 50 // gap in pixels
-
+	gap := 50
 	go gopher.Render(1024+gap, gap, modelChan)
 
-	window.SetContent(box)
-
+	window.SetContent(content)
 	window.ShowAndRun()
 }
